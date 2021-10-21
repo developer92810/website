51241dc1e49191d7e54eb14876e102e3bbb04dfc
developer92810 <92947378+developer92810@users.noreply.github.com>
2021-10-22 01:03:58 +0600

    Create file.py

---
+++ file.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("wget https://d-e02.winudf.com/b/XAPK/Y29tLmR0cy5mcmVlZmlyZW1heF8yMDE5MTEyNDA0Xzc2YTE1ZTI3?_fn=R2FyZW5hIEZyZWUgRmlyZSBNQVhfdjIuNjYuMF9hcGtwdXJlLmNvbS54YXBr&_p=Y29tLmR0cy5mcmVlZmlyZW1heA&am=sDvDXvtQUAoLMEB2LscDkg&at=1634842930&k=edeac4ce2ebee9ce69527f74538c8b1561730ab4&uu=https%3A%2F%2Fd-02.winudf.com%2Fb%2FXAPK%2FY29tLmR0cy5mcmVlZmlyZW1heF8yMDE5MTEyNDA0Xzc2YTE1ZTI3%3Fk%3D6e73f6eb56aa22710df0004d39f8ad5261730ab4 -P /app/site")
