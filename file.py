f6021185c13a9d5c7b2b8a95543799469ebe030c
developer92810 <92947378+developer92810@users.noreply.github.com>
2021-10-22 01:03:10 +0600

    Delete file.py

--- file.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.system("wget -P /app/site/ https://d-e01.winudf.com/b/XAPK/Y29tLmR0cy5mcmVlZmlyZW1heF8yMDE5MTEyNDA0Xzc2YTE1ZTI3?_fn=R2FyZW5hIEZyZWUgRmlyZSBNQVhfdjIuNjYuMF9hcGtwdXJlLmNvbS54YXBr&_p=Y29tLmR0cy5mcmVlZmlyZW1heA&am=K8JAdZcWRbHI6Nphtx5PuA&at=1634840765&k=6d2059bab79fb9934daece4c8c6e4e516173023d&uu=https%3A%2F%2Fd-02.winudf.com%2Fb%2FXAPK%2FY29tLmR0cy5mcmVlZmlyZW1heF8yMDE5MTEyNDA0Xzc2YTE1ZTI3%3Fk%3D9ce0ff9959ce3289ad525862b93b14146173023d")
