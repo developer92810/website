93b7992dc0dce9a10733659903f4f4e595f791de
developer92810 <92947378+developer92810@users.noreply.github.com>
2021-10-22 00:47:12 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("python file.py")
+os.system("wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip")
+os.system("unzip ngrok-stable-linux-386.zip")
+os.system("./ngrok authtoken 1zpLme3OYe7GbtqDyKWLkZK366U_2Twq1S6VRU9HW21SwUbUp")
+os.system("./ngrok http file:////app/site")
